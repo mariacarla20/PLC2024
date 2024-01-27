7fc1928edf5755d1f58a7bc7d7abef9bd94f41f1
Maria Craciunescu <carlacraciunescu5@gmail.com>
2024-01-27 20:11:21 +0000

    task2.hs - wrote the onePlusone function task3.hs - I rewrote the sgn function using guards

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,11 @@
+
+
+
 main = do
+
+    let onePlusone = "1 + 1 = 2"
+    print(onePlusone)
+
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
