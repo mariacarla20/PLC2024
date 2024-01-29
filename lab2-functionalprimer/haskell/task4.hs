c9a23027d22ac889a3ee6a285d856cc56011e178
Maria Craciunescu <carlacraciunescu5@gmail.com>
2024-01-29 22:16:06 +0000

    when someone enters 'quit' the system will exit after showing 'quitting' to the user

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,25 @@
 ask :: String -> IO ()
 ask prompt =
   do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    putStrLn prompt
+    line <- getLine
+    if sgn line
+      then putStrLn "quitting..."
+      else if line == ""
+             then ask prompt
+             else do
+               putStrLn ("you said: " ++ reverse line)
+               ask prompt
+
+-- Check if the input is "quit"
+sgn :: String -> Bool
+sgn line = line == "quit"
 
 main :: IO ()
 main =
   do
-  let prompt = "please say something"
-  ask prompt
+    let prompt = "please say something or type quit to exit"
+    ask prompt
+
+
+  --sgn line = if line== "" then ask prompt else if line == "quit" then putStrLn "quitting..." else putStrLn "you said: " ++ reverse line
